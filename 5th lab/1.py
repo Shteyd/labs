63de45746732404e0ea63668b09a8b0f30db96ec
Nikita Shteyd <shteyd.dev@gmail.com>
2021-03-24 22:14:52 +0300

    wip

--- 5th lab/1.py
+++ 5th lab/1.py
@@ -1,3 +1,9 @@
 import os
 
-print(len(os.listdir(input("Dir: "))))
+
+choice = input("Вы хотите ввести свою директорию: [Y]-Да [N]-Нет\n  > ")
+
+if choice == 'Y':
+    print(len(os.listdir(input("Введите директорию: "))))
+else:
+    print(len(os.listdir("C:\\Users\Shteyd\Downloads")))
