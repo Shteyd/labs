2b7a4ad778df2cbc354b25c1f72af57a9eab24b7
Nikita Shteyd <63547135+Shteyd@users.noreply.github.com>
2021-03-29 23:52:32 +0300

    Delete 5th lab directory

--- 5th lab/1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-choice = input("Вы хотите ввести свою директорию: [Y]-Да [N]-Нет\n  > ")
-
-if choice == 'Y':
-    print(len(os.listdir(input("Введите директорию: "))))
-else:
-    print(len(os.listdir("C:\\Users\Shteyd\Downloads")))
